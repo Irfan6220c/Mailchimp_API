4b6f16827c5dd96410a8073b19ef7cf519b188eb
Irfan6220c <ge69xud@mytum.de>
2021-03-03 22:32:11 +0100

    Signup page with mailchimp

---
+++ app.js
@@ -0,0 +1,81 @@
+
+const  express= require("express");
+const bodyParser=require ("body-parser");
+const http=require("https")
+
+var app=express();
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static("public"));
+
+
+
+app.get("/",function(req,res){
+
+    res.sendFile(__dirname+"/signup.html")
+
+});
+
+
+app.post("/",function(req,res)
+
+{
+
+    var fname=req.body.FirstName;
+    var lname=req.body.LastName;
+    var email=req.body.email;
+    var email=req.body.password;
+
+    var data={
+
+        members:[
+       {
+        email_address:email,
+        status: "subscibed",
+        merge_fields: {
+
+            FNAME:fname,
+            LNAME:lname
+        }
+
+    }]};
+
+    var jsonData=JSON.stringify(data);
+
+    const url=   "https://us1.api.mailchimp.com/3.0/lists/1af5cef1ed"
+
+    const options= {
+
+        method:"POST",
+        auth: "Irfan:1eee2b819a60032ee0dd5f47f1daaec8-us1"
+    }
+
+    const request=http.request(url,options,function(response){
+
+        response.on("data",function(data){
+
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+
+});
+
+    
+
+
+
+app.listen(3000,function(){
+
+console.log("Listening");
+
+});
+
+//API key
+
+//1eee2b819a60032ee0dd5f47f1daaec8-us1
+//List ID
+//1af5cef1ed
